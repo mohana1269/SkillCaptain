32b5bd42714c3b1e289dbe35f29cd82392224406
mohana1269 <116233411+mohana1269@users.noreply.github.com>
2024-03-23 22:39:08 +0000

    Add files via upload

---
+++ Java 1/Day3.java
@@ -0,0 +1,20 @@
+public class Day3 {
+
+     private int intVariable= 42;
+     private double doubleVariable= 3.14159f;
+     private char charCharacter='A';
+     private boolean booleanVariable=true;
+     String stringValue= "Hello,Skill Captain!";
+
+
+    public static void main(String[] args) {
+             Day3 m = new Day3();
+
+            System.out.println("Integer Variable " + m.intVariable);
+            System.out.println("Double Variable " + m.doubleVariable);
+            System.out.println("Character Variable " + m.charCharacter);
+            System.out.println("Boolean Variable " + m.booleanVariable);
+            System.out.println("String Variable " +m.stringValue);
+        }
+    }
+
